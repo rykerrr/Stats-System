af1c689717b37046f747c22551dc141137c1b781
Alex <w4llcr00ft@gmail.com>
2021-07-24 18:47:43 +0200

    ui attempt

---
+++ Assets/Scripts/Editor/BlurPanelEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+using UnityEditor.UIElements;
+
+#pragma warning disable 0649
+[CustomEditor(typeof(BlurPanel))]
+public class BlurPanelEditor : ImageEditor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        EditorGUI.BeginChangeCheck();
+        
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("time"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("delay"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("step"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("animate"));
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/BlurPanel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#pragma warning disable 0649
+[RequireComponent(typeof(CanvasGroup))]
+public class BlurPanel : Image
+{
+    [SerializeField] private float time = 0.5f;
+    [SerializeField] private float delay = 0f;
+    [SerializeField] private int step = 1;
+    [SerializeField] private bool animate;
+
+    private CanvasGroup canvGroup = default;
+
+    private WaitForEndOfFrame waitForFrame = new WaitForEndOfFrame();
+    private static readonly int Size = Shader.PropertyToID("_Size");
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        canvGroup = GetComponent<CanvasGroup>();
+    }
+    
+    protected override void OnEnable()
+    {
+        if (Application.isPlaying)
+        {
+            material.SetFloat(Size, 0);
+            canvGroup.alpha = 0;
+            StartCoroutine(Tween(0, 1));
+        }
+    }
+
+    private IEnumerator Tween(int a, int b)
+    {
+        for (int i = a; i < b; i += step)
+        {
+            material.SetFloat(Size, (float)b / i);
+            yield return waitForFrame;
+        }
+    }
+}
